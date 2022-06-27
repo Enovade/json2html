e4859c691f46a385c6717712b28a05fe0b38f6fe
Hendri Harmoko <hendri.harmoko@dhl.com>
2022-06-27 10:55:51 +0800

    v2

--- server.js
+++ server.js
@@ -7,23 +7,32 @@ const template = require("./template.json");
 
 app.use(express.json());
 
-// json2html.component.add("imgalts", {
-//   "<>": "tr",
-//   html: [
-//     { "<>": "td", html: "${imgsrc}" },
-//     { "<>": "td", html: "${imgalt}" },
-//   ],
-// });
-
 app.get("/", (req, res) => {
   res.send("Hello from JSON2HTML!");
 });
 
 app.post("/api/json2html", (req, res) => {
-  //   res
-  //     .status(200)
-  //     .send(json2html.render(req.body, getTemplate({ imgalt: "aaaa" })));
-  res.status(200).send(json2html.render(req.body, template));
+  const body = req.body
+  let htmlRes = '<body><table border="1">'
+  for (const key in body) {
+    htmlRes = htmlRes + '<tr>'
+    if (body.hasOwnProperty(key)) {
+      if(Array.isArray(body[key])){
+        htmlRes = htmlRes + `<td>${key}</td><td><table border="2">`
+        body[key].forEach((a, i)=>{
+          htmlRes = htmlRes + `<tr><td>${i}</td><td><img src="${a.imgsrc}"/></td></tr>`
+          console.log(a.imgsrc)
+        })
+        htmlRes = htmlRes + `</table>`
+      }
+      else{
+        htmlRes = htmlRes + `<td>${key}</td><td>${body[key]}</td>`
+      }
+        
+    }
+  }
+  htmlRes = htmlRes + `</table></body>`
+  res.send(htmlRes)
 });
 
 const port = process.env.PORT;
@@ -31,22 +40,3 @@ app.listen(port, () => {
   console.log(`Listening on port: ${port}...`);
 });
 
-// const json2html = require('node-json2html');
-
-// let data = {
-//     "employees":[
-//         {"name":"dorian"},
-//         {"name":"monica"},
-//         {"name":"jill"},
-//         {"name":"ashley"}
-//     ]
-// };
-
-// let template = {'<>':'ul','html':[
-//     {'<>':'li','obj':function(){return(this.employees)},'html':[
-//         {'<>':'span','text':'${name}'}
-//     ]}
-// ]};
-
-// //render and output
-// console.log( json2html.render(data,template) );
