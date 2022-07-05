53136a6903e9087c5a84bee770deefcb6e4ecd40
Hendri Harmoko <hendri.harmoko@dhl.com>
2022-07-05 16:07:42 +0700

    json parse

--- dist/index.js
+++ dist/index.js
@@ -2144,7 +2144,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(152);
 
 const json = core.getInput('json', { required: true });
-const body = json;
+const body = JSON.parse(json);
 let htmlRes = '<!DOCTYPE html><html lang="en">';
 htmlRes +=
   '<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script></head>';
