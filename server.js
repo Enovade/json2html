5a0fe8d4f1b5fd5962230e1493ad30ab66954c73
Hendri Harmoko <hendri.harmoko@dhl.com>
2022-07-05 17:13:44 +0700

    remove doctype

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 
 const json = core.getInput('json', { required: true });
 const body = JSON.parse(json);
-let htmlRes = '<!DOCTYPE html><html lang="en">';
+let htmlRes = '<html lang="en">';
 htmlRes +=
   '<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script></head>';
 htmlRes +=
